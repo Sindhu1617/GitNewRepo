bb194bb7913b7d21860e92ef3573f183eb3f785c
Sindhu1617 <sindhunandinee@gmail.com>
2022-01-08 08:58:28 +0530

    Added new class Demo to SeleniumMorningClass project

---
+++ SeleniumMorningClass/src/icici/loan/carloans/Demo.java
@@ -0,0 +1,10 @@
+package icici.loan.carloans;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I m newly created");
+	}
+
+}
