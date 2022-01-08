4fc1f43181ec395e629f53d7d905c0cce24f2006
Sindhu1617 <sindhunandinee@gmail.com>
2022-01-08 09:03:25 +0530

    Added second line in Demo class

--- SeleniumMorningClass/src/icici/loan/carloans/Demo.java
+++ SeleniumMorningClass/src/icici/loan/carloans/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I m newly created");
+		System.out.println("Second line");
 	}
 
 }
