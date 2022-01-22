aee75ca575145f4d034c367ae9f5352635758b21
Sindhu1617 <sindhunandinee@gmail.com>
2022-01-22 08:36:37 +0530

    Reverted

--- NewGitTrial/src/newOne/Day2Git.java
+++ NewGitTrial/src/newOne/Day2Git.java
@@ -4,7 +4,7 @@ public class Day2Git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("New  class");
+		System.out.println("New class");
 	}
 
 }
